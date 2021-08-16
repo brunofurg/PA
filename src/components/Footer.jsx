41a09ec92c69cdf1118518e867a7500ac7e1cc39
Bruno FURG <brunosduarte@furg.br>
2021-08-16 02:16:53 -0300

    updates09

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer({ children }) {
   return (
     <footer>
-      <div className="bg-gray-200 hover:bg-secondary ">
+      <div className="bg-gray-200">
         <h6 className={`text-center fontsize-pp`}>Desenvolvido por BDSystems ® 2021</h6>
       </div>
     </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,20 +11,20 @@ export default function Header({acao}) {
        <span> 
          <img src={imgLogo} alt="PortoNaTela" width="75" height="75"/>
        </span>
-       <span className="border-2 border-indigo-400 rounded p-2">
+       <span className="border-2 border-gray-300 rounded-md p-2">
         <strong>
           17ºC - 16:20
         </strong>
       </span>
 
        <span className="flex flex-row space-x-4 text-lg">
-          <span className="border-2 rounded-full p-2"> 
+          <span className="border-2 border-gray-300 rounded-full p-2 hover:bg-secondary "> 
              <img src={iconNotifications} alt="Notifications" width="25" height="25"/>
            </span>
-           <span className="border-2 rounded-full p-2"> 
+           <span className="border-2 border-gray-300 rounded-full p-2 hover:bg-secondary "> 
              <img src={iconUser} alt="User" width="25" height="25"/>
            </span>
-           <span className="border-2 rounded-full p-2"> 
+           <span className="border-2 border-gray-300 rounded-full p-2 mr-4 hover:bg-secondary "> 
              <img src={iconMenu} alt="Menu" width="25" height="25"/>
            </span>
         </span>
